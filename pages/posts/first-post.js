733272ee6953d5e5cc3b076c276dc7da214057a7
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-10-14 09:37:53 +0100

    added styling components

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,19 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back Home</Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back Home</Link>
+        </h2>
+      </Layout>
     </>
   );
 }
